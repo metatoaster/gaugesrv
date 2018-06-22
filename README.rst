ed3b163539b86639011a11537e0b3d7037b85d97
Tommy Yu <y@metatoaster.com>
2018-06-22 12:36:43 +0100

    Rough demonstration of gaugesrv
    - Implement the basic glue between jQuery and gaugeJS, based on the demo
      code provided on the gaugeJS project website and the local data nodes
      that triggers it.
    - Instructions to get started added to README also.

--- README.rst
+++ README.rst
@@ -2,3 +2,13 @@ gaugesrv
 ========
 
 A simple gauge web service/visualization using gauge.js.
+
+Currently under development.  The way to get this demo running is
+roughly the following:
+
+.. code::
+
+    $ git clone https://github.com/metatoaster/gaugesrv.git
+    $ pip install -e .[dev,webpack,scss]
+    $ calmjs npm --install -i gaugesrv[dev,webpack,scss]
+    $ python setup.py build && python -m gaugesrv.app
--- src/gaugesrv/lib.js
+++ src/gaugesrv/lib.js
@@ -1,2 +1,33 @@
 var $ = require('jquery');
 var Gauge = require('gaugeJS').Gauge;
+
+
+$.fn.gaugesrv = function() {
+    this.each(function() {
+        var $this = $(this),
+            data = $this.data();
+
+        if (data.gauge) {
+            delete data.gauge;
+        }
+        if (data.gaugesrvOpts !== false) {
+            data.gauge = new Gauge(this).setOptions(data.gaugesrvOpts);
+            data.gauge.minValue = data.gaugeMin || data.gauge.minValue;
+            data.gauge.maxValue = data.gaugeMax || data.gauge.maxValue;
+            data.gauge.set(data.gaugeValue || 0);
+        }
+    });
+    return this;
+};
+
+
+var main = function() {
+    $('canvas[data-gaugesrv-opts]').each(function(idx) {
+        $(this).gaugesrv();
+    });
+};
+
+
+exports.main = main;
+
+window.document.addEventListener('DOMContentLoaded', main);
